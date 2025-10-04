5561afbfb5fa283dfdc8c7fff7c01cabfb1dd684
Eyuti272 <Eyuti272@gmail.com>
2025-10-04 15:31:11 +0300

    i fixed .env part

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -1,23 +1,23 @@
 const mysql2 = require("mysql2");
-// require("dotenv").config();
+require("dotenv").config();
 
-// const dbconnection = mysql2.createPool({
-//   user: process.env.DB_USER,
-//   database: process.env.DB_DATABASE,
-//   host: process.env.DB_HOST,
-//   password: process.env.DB_PASSWORD,
+const dbconnection = mysql2.createPool({
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
  
-// });
+});
 
-const dbconnection=mysql2.createPool({
-    user:"Eyobs",
-    database:"evangadi-forum-practice",
-    password:"87654321",
-    host:"localhost",
-    port:"8889"
+// const dbconnection=mysql2.createPool({
+//     user:"Eyobs",
+//     database:"evangadi-forum-practice",
+//     password:"87654321",
+//     host:"localhost",
+//     port:"8889"
     
 
-})
+// })
 
 
 
